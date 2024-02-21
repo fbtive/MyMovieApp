7a5d3ae7ba668b5283ecedd0c0c35149eef3b68c
Felix Bello <bello2redfield@gmail.com>
2024-02-21 18:00:44 +0700

    project setup and initial commit

---
+++ app/src/main/java/com/example/mymovieapp/ui/ActivityMain.kt
@@ -0,0 +1,13 @@
+package com.example.mymovieapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mymovieapp.R
+
+class ActivityMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
